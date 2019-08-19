544742ebc42c007fdbd1b44182eab614cf949c82
javascriptFan <wdeveloping9099@gmail.com>
2019-08-19 06:05:25 +0400

    reset pagesetting on install

--- server/install.js
+++ server/install.js
@@ -25,16 +25,7 @@ async function installing(ctx) {
             shops[0].exitIntent = false;
             shops[0].exitIntentTime = 5;
             shops[0].install = 1;
-            shops[0].pageSetting = `{
-                homepage: false,
-                allStatic: false,
-                allCollections: false,
-                allProducts: false,
-                allBlogs: false,
-                cart: false,
-                search: false,
-                saveDisabled: false
-            }`;
+            shops[0].pageSetting =  `{ "homepage": false,"pages": {"allPages": false},"products": {"allProducts": false},"blogs": {"allBlogs": false},"cart": false,"search": false}`;
             shops[0].save();
             id = shops[0].id;
         } else {
