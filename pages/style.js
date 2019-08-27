3bdeddba155912338530200d32cb6f2d110fe153
javascriptFan <wdeveloping9099@gmail.com>
2019-08-28 03:54:25 +0400

    init style css

--- pages/style.js
+++ pages/style.js
@@ -1,18 +1,28 @@
 import { Checkbox, Button, Select, Stack, Card } from '@shopify/polaris';
 import store from 'store-js';
 import Cookies from 'js-cookie';
-import '../stylesheets/settings.css';
+import '../stylesheets/style.css';
 import '../stylesheets/global.css';
 
 class Style extends React.Component {
   state = {
       style: '',
       options : [
-        {label: 'Color 1', value: '#eae56f'},
-        {label: 'Color 2', value: '#89f26e'},
-        {label: 'Color 3', value: '#e7706f'},
+        {label: 'Dark Skin', value1: '#000000', value2: '#ffffff'},
+        {label: 'Tada Skin', value1: '#9a55d6', value2: '#ff5c6d'},
+        {label: 'Default Skin', value1: '#f8921f', value2: '#29aae3'},
+        {label: '1 Skin', value1: '#9f4141', value2: '#ef8180'},
+        {label: '2 Skin', value1: '#9574e9', value2: '#efd895'},
+        {label: '3 Skin', value1: '#6dbf91', value2: '#1d672c'},
+        {label: '4 Skin', value1: '#ff7d01', value2: '#999999'},
+        {label: '5 Skin', value1: '#f0a9af', value2: '#846b89'},
+        {label: '6 Skin', value1: '#3c493f', value2: '#b8dad9'},
+        {label: '7 Skin', value1: '#e9b9ab', value2: '#967796'},
+        {label: '8 Skin', value1: '#0170c1', value2: '#8fe3ee'},
+        {label: '9 Skin', value1: '#f59599', value2: '#f26970'}
       ],
-      selected: '#eae56f'
+      selected: {label: 'Dark Skin', value1: '#000000', value2: '#ffffff'},
+      isDropdown: false
     };
 
   componentDidMount = () => {
@@ -34,7 +44,7 @@ class Style extends React.Component {
         this.setState({
             style
         })
-        if(style != '#dddddd' && style != '#333333') {
+        if(style != '#ffffff' && style != '#333333') {
             this.setState({
                 selected: style
             })
@@ -43,25 +53,41 @@ class Style extends React.Component {
   }
 
   render() {
-      const { style, options, selected } = this.state;
+    const { style, options, selected, isDropdown } = this.state;
     return (
-      <div>
+      <div className="style-content">
         <Card>
             <div className="header3">Visual Style</div>
-            <div>
-                <Checkbox checked={style == '#dddddd'} label="Clear Theme" onChange={() =>this.changeColor('#dddddd')} />
+            <div className="default-color">
+                <Checkbox checked={style == '#ffffff'} label="Clear Theme" onChange={() =>this.changeColor('#ffffff')} />
             </div>
-            <div>
+            <div className="default-color">
                 <Checkbox checked={style == '#333333'} label="Dark Theme" onChange={() => this.changeColor('#333333')} />
             </div>
-            <Stack horizontal>
-                <Checkbox checked={style != '#dddddd' && style != '#333333'} label="Color Theme" onChange={() => this.changeColor('color')} />
-                <Select
-                    options={options}
-                    onChange={this.handleChange}
-                    value={selected}
-                />
-            </Stack>
+            <div className="default-color">
+                <Checkbox checked={style != '#ffffff' && style != '#333333'} label="Color Theme" onChange={() => this.changeColor('color')} />
+            </div>
+            <div className="color-selector">
+                <div className="color-select" onClick={this.showDropdown}>
+                    <span className="color-name">{selected.label}</span>
+                    <span className="color-pattern" style={{backgroundColor: selected.value1}}></span>
+                    <span className="color-pattern" style={{backgroundColor: selected.value2}}></span>
+                    <span class="Polaris-Select__Icon"><span class="Polaris-Icon"><svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true"><path d="M13 8l-3-3-3 3h6zm-.1 4L10 14.9 7.1 12h5.8z" fill-rule="evenodd"></path></svg></span></span>
+                </div>
+                <div className="color-selector-dropdown" style={{display: (isDropdown)?'block':'none'}}>
+                    {
+                        options.map(option => (
+                            <div>
+                                <div className="color-name">
+                                    {option.label}
+                                </div>
+                                <div className="color-pattern" style={{backgroundColor: option.value1}}></div>
+                                <div className="color-pattern" style={{backgroundColor: option.value2}}></div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </Card>
         <div className="coupon-bottom">
             <div className="coupon-prev-btn-wrapper">
@@ -75,9 +101,15 @@ class Style extends React.Component {
     )
   }
 
+  showDropdown = () => {
+      this.setState({
+          isDropdown: !this.state.isDropdown
+      })
+  }
+
   changeColor = (value) => {
       var result = value;
-      if(value == '#dddddd' || value == '#333333') {
+      if(value == '#ffffff' || value == '#333333') {
         this.setState({
             style: value
         })
@@ -101,7 +133,7 @@ class Style extends React.Component {
   }
 
   handleChange = (value) => {
-      if(value == '#dddddd' || value == '#333333') {
+      if(value == '#ffffff' || value == '#333333') {
           this.setState({
               selected: value
           })
