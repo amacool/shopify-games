2c951e193c5689f0c04dd323b34c8f6cccebd07b
Cool Dev <amacool0117@outlook.com>
2019-09-08 21:52:43 +0300

    update font style

--- server/frontend.js
+++ server/frontend.js
@@ -222,6 +222,7 @@ function selectWidgetBySetting(widget) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="${widget_url}/custom.js"></script>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
 
     <div id="tada_app_widget">
       <div id="spinny_box">
