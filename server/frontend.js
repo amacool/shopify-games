0e0bf1d53c2e030bf8f2dac45aefca9a2ca5b973
Cool Dev <amacool0117@outlook.com>
2019-11-10 01:05:10 +0200

    update meta link

--- server/frontend.js
+++ server/frontend.js
@@ -241,10 +241,7 @@ function selectWidgetBySetting(widget) {
   if(widget.type === 0) {
     var id = widget.id;
     html = `
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="apple-touch-fullscreen" content="yes" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <script>
       window.global_widget_url = "${widget_url}";
       window.game_start_icon_position = "${game_start_icon_position}";
